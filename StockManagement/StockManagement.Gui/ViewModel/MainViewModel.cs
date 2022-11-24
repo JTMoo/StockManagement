ed2b4e095c7d1323193d55023b9a670aae9e9722
Jonathan Trefz <jonathan.trefz@t-online.de>
2022-11-23 21:28:43 -0300

    NotificationBase implemented

--- StockManagement/StockManagement.Gui/ViewModel/MainViewModel.cs
+++ StockManagement/StockManagement.Gui/ViewModel/MainViewModel.cs
@@ -2,15 +2,14 @@
 using System.Windows;
 
 
-namespace StockManagement.Gui.ViewModel
-{
-    internal class MainViewModel
-    {
-        public MainViewModel()
-        {
-            this.QuitCommand = new RelayCommand<string>(_ => Application.Current.Shutdown());
-        }
+namespace StockManagement.Gui.ViewModel;
 
-        public RelayCommand<string> QuitCommand { get; }
+internal class MainViewModel
+{
+    public MainViewModel()
+    {
+        this.QuitCommand = new RelayCommand<string>(_ => Application.Current.Shutdown());
     }
+
+    public RelayCommand<string> QuitCommand { get; }
 }
