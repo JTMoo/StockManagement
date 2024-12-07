a09e5cdc33114836d3157866eb8960524ff1c168
Jonathan <65012136+JTMoo@users.noreply.github.com>
2024-12-07 08:23:00 -0300

    UI revamp colors and fonts
    - Added new font to the project
    - Added a control that displays a preview
    - changed colors for nearly every view/dialog

---
+++ StockManagement.Gui/View/Controls/TextBoxWithPreviewControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StockManagement.Gui.View.Controls;
+
+
+public partial class TextBoxWithPreviewControl : UserControl
+{
+	public static readonly DependencyProperty ActualTextProperty = DependencyProperty.Register(
+		"ActualText",
+		typeof(string),
+		typeof(TextBoxWithPreviewControl));
+	public static readonly DependencyProperty TextPreviewProperty = DependencyProperty.Register(
+		"TextPreview",
+		typeof(string),
+		typeof(TextBoxWithPreviewControl));
+
+	public TextBoxWithPreviewControl()
+	{
+		InitializeComponent();
+	}
+
+	public string ActualText
+	{
+		get => (string)GetValue(ActualTextProperty);
+		set => SetValue(ActualTextProperty, value);
+	}
+
+	public string TextPreview
+	{
+		get => (string)GetValue(TextPreviewProperty);
+		set => SetValue(TextPreviewProperty, value);
+	}
+}
