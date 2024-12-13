cb04e82cb050d2205da9abc3d9ad881f170ad589
Jonathan <65012136+JTMoo@users.noreply.github.com>
2024-12-13 13:47:20 -0300

    Added UI Element
    - Added larger version of the textbox with preview

---
+++ StockManagement.Gui/View/Controls/LargeTextBoxWithPreviewControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StockManagement.Gui.View.Controls;
+
+
+public partial class LargeTextBoxWithPreviewControl : UserControl
+{
+	public static readonly DependencyProperty ActualTextProperty = DependencyProperty.Register(
+		"ActualText",
+		typeof(string),
+		typeof(LargeTextBoxWithPreviewControl));
+	public static readonly DependencyProperty TextPreviewProperty = DependencyProperty.Register(
+		"TextPreview",
+		typeof(string),
+		typeof(LargeTextBoxWithPreviewControl));
+
+	public LargeTextBoxWithPreviewControl()
+	{
+		InitializeComponent();
+	}
+
+	public string ActualText
+	{
+		get => (string)GetValue(ActualTextProperty);
+		set => SetValue(ActualTextProperty, value);
+	}
+
+	public string TextPreview
+	{
+		get => (string)GetValue(TextPreviewProperty);
+		set => SetValue(TextPreviewProperty, value);
+	}
+}
